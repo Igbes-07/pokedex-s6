c3c261a6a0bdde3f3d396f6e60884bcd03443b2e
WazabiH <hamedwassila2004@gmail.com>
2026-06-23 14:48:11 +0200

    correction chemin admin.Css

--- admin/upload.php
+++ admin/upload.php
@@ -78,7 +78,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin - Upload Jaquettes | Pokédex</title>
-    <link rel="stylesheet" href="./admin.css">
+    <link rel="stylesheet" href="../src/styles/admin.css">
 </head>
 <body>
 
